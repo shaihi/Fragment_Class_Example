9baf1911bf02e6ee35b001108cd5b50a2c95dc44
shaihi <shaihi@icloud.com>
2024-05-07 10:15:04 +0300

    Added one fragment and loaded it in MainActivity

---
+++ app/src/main/java/com/shaihi/classexample/FirstFragment.java
@@ -0,0 +1,52 @@
+package com.shaihi.classexample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FirstFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FirstFragment extends Fragment {
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static FirstFragment newInstance() {
+        FirstFragment fragment = new FirstFragment();
+        //Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        //fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        TextView tv = view.findViewById(R.id.tvFragOne);
+        tv.setText("HELLO 1");
+        return view;
+    }
+}

fc4083c5d72a206feaaab32622abb5c39e7cf477
shaihi <shaihi@icloud.com>
2024-05-07 10:19:15 +0300

    Added second fragment

---
+++ app/src/main/java/com/shaihi/classexample/SecondFragment.java
@@ -0,0 +1,43 @@
+package com.shaihi.classexample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SecondFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    public static SecondFragment newInstance() {
+        SecondFragment fragment = new SecondFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+}
